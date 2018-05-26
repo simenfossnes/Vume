28a9887cdc7441b9f46090fcae44d3c3cb0b939e
simenfossnes <simenf13@student.uia.no>
2018-05-26 14:34:50 +0200

    Added home container, reorganized files, folder structure, react router, +more.

---
+++ ui(front-end)/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './containers/Home';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <Router>
+            <Route exact path="/" component={Home}/>
+            {/*<Route exact path="/session/:sessionId" component={Session}/>*/}
+        </Router>
+    );
+  }
+}
+
+export default App;
