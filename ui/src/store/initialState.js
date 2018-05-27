05eb03fd3d5d0f1a04ee74175978d3e4bee15aa7
simenfossnes <simenf13@student.uia.no>
2018-05-27 05:46:43 +0200

    Added redux, react-redux, redux-thunk, and set up initalState.

---
+++ ui/src/store/initialState.js
@@ -0,0 +1,19 @@
+export default {
+    entities: {
+        shrooms: {
+            byId: {},
+            allIds: [],
+            selected: '',
+        },
+        comments: {
+            byId: {},
+            allIds: [],
+        }
+    },
+    scenes: {
+        home: {},
+        shroomCreator: {},
+        viewerShroom: {},
+        presenterShroom: {}
+    }
+}
