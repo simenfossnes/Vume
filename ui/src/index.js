17a67c7ec62eb2944139057defb7262596e63811
simenfossnes <simenf13@student.uia.no>
2018-05-27 06:15:36 +0200

    configureStore setup, redux successfully setup and is now up and running.

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,6 +4,14 @@ import './normalize.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
