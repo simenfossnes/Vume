ca0b4df1012672477b0daa24dabca1f91f20dc9a
simenfossnes <simenf13@student.uia.no>
2018-05-27 12:57:24 +0200

    Timer updated.

--- ui/src/components/vume-base-components/Timer/Timer.js
+++ ui/src/components/vume-base-components/Timer/Timer.js
@@ -7,15 +7,33 @@ class Timer extends React.Component {
 
     state = {
         startTime: Date.now(),
-        difference: Date.now()
+        difference: '00:00:00'
     };
 
     timer = () => {
-        this.setState((prevState) => ({difference: Date.now() - prevState.startTime}));
+        this.setState((prevState) => ({difference: this.msToTime(Date.now() - prevState.startTime)}));
     };
 
     componentDidMount() {
-        setInterval(this.timer, 50)
+        this.clock = setInterval(this.timer, 1000);
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.clock);
+    }
+
+    // helper function taken from stackoverflow:
+    msToTime = (duration) => {
+        let milliseconds = parseInt((duration%1000)/100)
+            , seconds = parseInt((duration/1000)%60)
+            , minutes = parseInt((duration/(1000*60))%60)
+            , hours = parseInt((duration/(1000*60*60))%24);
+
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+        return hours + ":" + minutes + ":" + seconds;
     };
 
     render() {
